995dfb92c9666d680d0eb34e25ae93d71a799e8d
Tioluwani Enoch <codemaestro101@gmail.com>
2024-05-29 16:09:40 +0100

    improved user-interface

--- public/js/more-details.js
+++ public/js/more-details.js
@@ -1,6 +1,7 @@
 const COUNTRY_CONTAINER = document.getElementById("country-container");
 const COUNTRY = new URLSearchParams(window.location.search).get("country");
 const LOADER = document.getElementById("loader");
+const THEME = localStorage.getItem("theme");
 
 let loaded = false;
 
@@ -13,68 +14,101 @@ async function fetchData() {
     DATA.map((country) => {
       LOADER.style.display = "none";
       COUNTRY_CONTAINER.innerHTML += `
-      <div class="w-1/3">
-      <img src="${country.flags.svg}" alt="${country.flags.alt}" class="w-full h-full" />
-    </div>
-    <div class="w-2/3 py-6 flex flex-col gap-10 justify-center">
-      <h2 class="font-bold text-2xl">${country.name.common}</h2>
-      <div class="flex gap-36">
-        <div class="flex flex-col gap-2">
-          <p class="text-md">
-            <span class="font-bold"> Native Name:</span>
-          </p>
-          <p class="text-md">
-            <span class="font-bold"> Population:</span>
-            ${country.population}
-          </p>
-          <p class="text-md">
-            <span class="font-bold"> Region:</span>
-            ${country.region}
-          </p>
-          <p class="text-md">
-            <span class="font-bold">Sub Region:</span>
-            ${country.subregion}
-          </p>
-          <p class="text-md">
-            <span class="font-bold"> Capital:</span>
-            ${country.capital}
-          </p>
+        <div class="w-full lg:w-1/3">
+        <img src="${country.flags.svg}" alt="${country.flags.alt}" class="w-full h-full" />
+      </div>
+      <div class="w-full lg:w-2/3 py-6 flex flex-col gap-10 justify-center">
+        <h2 class="font-bold text-2xl">${country.name.common}</h2>
+        <div class="flex-column md:flex-row gap-36 ">
+          <div class="flex flex-col gap-2">
+            <p class="text-md">
+              <span class="font-bold"> 
+                Native Name: 
+                ${Object.entries(country.name.nativeName)[0][1].common}
+              </span>
+            </p>
+            <p class="text-md">
+              <span class="font-bold"> Population:</span>
+              ${country.population}
+            </p>
+            <p class="text-md">
+              <span class="font-bold"> Region:</span>
+              ${country.region}
+            </p>
+            <p class="text-md">
+              <span class="font-bold">Sub Region:</span>
+              ${country.subregion}
+            </p>
+            <p class="text-md">
+              <span class="font-bold"> Capital:</span>
+              ${country.capital}
+            </p>
+          </div>
+          <div class="flex flex-col gap-2">
+            <p class="text-md">
+              <span class="font-bold"> Top Level Domain:</span>
+              ${country.tld[0]}
+            </p>
+            <p class="text-md">
+              <span class="font-bold"> Currencies:</span>
+              ${Object.entries(country.currencies)[0][1].name}
+            </p>
+            <p class="text-md flex flex-col lg:flex-row">
+              <span class="font-bold"> Languages:</span>
+              ${Object.entries(country.languages)[0][1]}
+            </p>
+          </div>
         </div>
-        <div class="flex flex-col gap-2">
-          <p class="text-md">
-            <span class="font-bold"> Top Level Domain:</span>
-            ${country.tld[0]}
-          </p>
-          <p class="text-md">
-            <span class="font-bold"> Currencies:</span>
-          </p>
-          <p class="text-md">
-            <span class="font-bold">Languages: </span>
-            ${country.languages.deu},
-            ${country.languages.fra},
-            ${country.languages.nld}
-          </p>
+        <div class="flex flex-col lg:flex-row items-start lg:items-center gap-4">
+          <p class="me-4 font-bold">Bored Countires:</p>
+          <div class="flex flex-col md:flex-row">
+          </div>
         </div>
       </div>
-      <div class="flex items-center gap-4">
-        <p class="me-4 font-bold">Bored Countires:</p>
-        <div class="px-6 py-2 flex shadow-md">
-          <span>France</span>
-        </div>
-        <div class="px-6 py-2 flex shadow-md">
-          <span>Germany</span>
-        </div>
-        <div class="px-6 py-2 flex shadow-md">
-          <span>Netherlands</span>
-        </div>
-      </div>
-    </div>
 
-      `;
+        `;
     });
+    themeHandler();
   } catch (error) {
     console.error(error);
   }
 }
 
 fetchData();
+
+const THEME_TOGGLE = document.getElementById("theme-toggle");
+const HEADER = document.querySelector("header");
+const H1 = document.querySelector("h1");
+const MAIN = document.querySelector("main");
+const COUNTRY_CONTAIN = document.getElementById("country-container");
+const BACK_BUTTON = document.getElementById("back-button");
+// const SELECT = document.querySelector("select");
+
+THEME_TOGGLE.addEventListener("click", () => {
+  if (!localStorage.getItem("theme") || localStorage.getItem("theme") === "light") {
+    localStorage.setItem("theme", "dark");
+  } else if (localStorage.getItem("theme") === "dark") {
+    localStorage.setItem("theme", "light");
+  }
+  themeHandler();
+});
+
+function themeHandler() {
+  if (localStorage.getItem("theme") === "dark") {
+    HEADER.classList.remove("bg-white");
+    HEADER.classList.add("bg-dark-blue");
+    H1.classList.add("text-white");
+    THEME_TOGGLE.classList.add("text-white");
+    MAIN.classList.remove("bg-lmbg-very-light-gray");
+    MAIN.classList.add("bg-dmbg-very-dark-blue");
+    MAIN.classList.add("text-white");
+  } else {
+    HEADER.classList.add("bg-white");
+    HEADER.classList.remove("bg-dark-blue");
+    H1.classList.remove("text-white");
+    THEME_TOGGLE.classList.remove("text-white");
+    MAIN.classList.add("bg-lmbg-very-light-gray");
+    MAIN.classList.remove("bg-dmbg-very-dark-blue");
+    MAIN.classList.remove("text-white");
+  }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,39 @@
 const COUNTRY_CONTAINER = document.getElementById("country-container");
 const LOADER = document.getElementById("loader");
 let COUNTRY_CARD;
-const THEME = localStorage.getItem("theme");
+// const THEME = localStorage.getItem("theme");
 let COUNTRY_NAME;
 
 async function fetchData() {
   try {
     const RESPONSE = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,region,population,capital");
     const DATA = await RESPONSE.json();
-    // console.log(DATA);
-
     DATA.map((country) => {
       LOADER.style.display = "none";
       COUNTRY_CONTAINER.innerHTML += `
-              <div class="bg-white w-full rounded-md shadow-md country-card">
-                  <div class="h-[230px]">
-                      <img src="${country.flags.png}" alt="${country.flags.alt}" class="rounded-t-md w-full h-full cursor-pointer" onclick="displayMoreInformation('${country.name.official}')"/>
-                  </div>
-                  <div class="h-1/2 flex flex-col gap-3 px-5 pt-5 pb-12 border-red-500">
-                      <h2 class="font-bold text-lg">${country.name.official}</h2>
-                      <div class="flex flex-col gap-2">
-                      <p>Population: ${country.population}</p>
-                      <p>Region: ${country.region}</p>
-                      <p>Capital: ${country.capital[0]}</p>
-                  </div>
-              </div>
+      <div class="bg-white w-full rounded-md shadow-md country-card">
+      <div class="h-[150px] md:h-[230px]">
+          <img src="${country.flags.png}" alt="${country.flags.alt}" class="rounded-t-md w-full h-full cursor-pointer" onclick="displayMoreInformation('${country.name.official}')"/>
+      </div>
+      <div class="h-1/2 flex flex-col gap-3 px-5 pt-5 pb-12">
+          <h2 class="font-bold text-lg">${country.name.official}</h2>
+          <div class="flex flex-col gap-2">
+          <p>Population: ${country.population}</p>
+          <p>Region: ${country.region}</p>
+          <p>Capital: ${country.capital[0]}</p>
+      </div>
+  </div>
       `;
-      // localStorage.setItem("country", `${country.name.common}`);
       COUNTRY_NAME = `${country.name.common}`;
     });
     COUNTRY_CARD = document.querySelectorAll(".country-card");
+    themeHandler();
   } catch (error) {
     console.error(error);
   }
 }
-
 fetchData();
+console.log(COUNTRY_CARD);
 
 const THEME_TOGGLE = document.getElementById("theme-toggle");
 const HEADER = document.querySelector("header");
@@ -45,29 +43,53 @@ const SEARCH_DIV = document.getElementById("search-div");
 const SELECT = document.querySelector("select");
 
 THEME_TOGGLE.addEventListener("click", () => {
-  if (THEME === "dark") {
-    localStorage.setItem("theme", "light");
-  } else if (THEME === "light") {
+  if (!localStorage.getItem("theme") || localStorage.getItem("theme") === "light") {
     localStorage.setItem("theme", "dark");
+  } else if (localStorage.getItem("theme") === "dark") {
+    localStorage.setItem("theme", "light");
   }
-  HEADER.classList.toggle("bg-white");
-  HEADER.classList.toggle("bg-dark-blue");
-  H1.classList.toggle("text-white");
-  THEME_TOGGLE.classList.toggle("text-white");
-  MAIN.classList.toggle("bg-lmbg-very-light-gray");
-  MAIN.classList.toggle("bg-dmbg-very-dark-blue");
-  SEARCH_DIV.classList.toggle("bg-white");
-  SEARCH_DIV.classList.toggle("text-white");
-  SEARCH_DIV.classList.toggle("bg-dark-blue");
-  SELECT.classList.toggle("bg-dark-blue");
-  SELECT.classList.toggle("bg-white");
-  SELECT.classList.toggle("text-white");
-  for (let CARD of COUNTRY_CARD) {
-    CARD.classList.toggle("bg-white");
-    CARD.classList.toggle("bg-dark-blue");
-    CARD.classList.toggle("text-white");
-  }
+  themeHandler();
 });
+
+function themeHandler() {
+  if (localStorage.getItem("theme") === "dark") {
+    HEADER.classList.remove("bg-white");
+    HEADER.classList.add("bg-dark-blue");
+    H1.classList.add("text-white");
+    THEME_TOGGLE.classList.add("text-white");
+    MAIN.classList.remove("bg-lmbg-very-light-gray");
+    MAIN.classList.add("bg-dmbg-very-dark-blue");
+    SEARCH_DIV.classList.remove("bg-white");
+    SEARCH_DIV.classList.add("text-white");
+    SEARCH_DIV.classList.add("bg-dark-blue");
+    SELECT.classList.add("bg-dark-blue");
+    SELECT.classList.remove("bg-white");
+    SELECT.classList.add("text-white");
+    for (let CARD of COUNTRY_CARD) {
+      CARD.classList.remove("bg-white");
+      CARD.classList.add("bg-dark-blue");
+      CARD.classList.add("text-white");
+    }
+  } else {
+    HEADER.classList.add("bg-white");
+    HEADER.classList.remove("bg-dark-blue");
+    H1.classList.remove("text-white");
+    THEME_TOGGLE.classList.remove("text-white");
+    MAIN.classList.add("bg-lmbg-very-light-gray");
+    MAIN.classList.remove("bg-dmbg-very-dark-blue");
+    SEARCH_DIV.classList.add("bg-white");
+    SEARCH_DIV.classList.remove("text-white");
+    SEARCH_DIV.classList.remove("bg-dark-blue");
+    SELECT.classList.remove("bg-dark-blue");
+    SELECT.classList.add("bg-white");
+    SELECT.classList.remove("text-white");
+    for (let CARD of COUNTRY_CARD) {
+      CARD.classList.add("bg-white");
+      CARD.classList.remove("bg-dark-blue");
+      CARD.classList.remove("text-white");
+    }
+  }
+}
 
 function displayMoreInformation(country) {
   window.location.href = `./more-details.html?country=${country}`;
@@ -114,4 +136,44 @@ SEARCH_FORM.addEventListener("submit", async (e) => {
   } catch (error) {
     console.error(error);
   }
+});
+
+const SELECT_FORM = document.getElementById("select-form");
+const FILTER_COUNTRY = document.getElementById("filter-country");
+
+FILTER_COUNTRY.addEventListener("input", () => {
+  const FILTER = FILTER_COUNTRY.options[FILTER_COUNTRY.selectedIndex].text;
+
+  async function fetchRegion() {
+    try {
+      const RESPONSE = await fetch(`https://restcountries.com/v3.1/region/${FILTER}`);
+      const DATA = await RESPONSE.json();
+      // console.log(DATA);
+      COUNTRY_CONTAINER.innerHTML = "";
+      DATA.map((country) => {
+        LOADER.style.display = "none";
+        COUNTRY_CONTAINER.innerHTML += `
+            <div class="bg-white w-full rounded-md shadow-md country-card">
+                <div class="h-[150px] md:h-[230px]">
+                    <img src="${country.flags.png}" alt="${country.flags.alt}" class="rounded-t-md w-full h-full cursor-pointer" onclick="displayMoreInformation('${country.name.official}')"/>
+                </div>
+                <div class="h-1/2 flex flex-col gap-3 px-5 pt-5 pb-12">
+                    <h2 class="font-bold text-lg">${country.name.official}</h2>
+                    <div class="flex flex-col gap-2">
+                    <p>Population: ${country.population}</p>
+                    <p>Region: ${country.region}</p>
+                    <p>Capital: ${country.capital[0]}</p>
+                </div>
+            </div>
+        `;
+        // localStorage.setItem("country", `${country.name.common}`);
+        COUNTRY_NAME = `${country.name.common}`;
+      });
+      COUNTRY_CARD = document.querySelectorAll(".country-card");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchRegion();
 });
